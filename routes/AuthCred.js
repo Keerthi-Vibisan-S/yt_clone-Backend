053469d18f94bf30e550327113dea1ec9021923b
KEERTHI VIBISAN S <keerthivibisan.ct20@bitsathy.ac.in>
2022-01-18 13:21:49 +0530

    Auth Post Req added & SignIn Complete

--- routes/AuthCred.js
+++ routes/AuthCred.js
@@ -4,11 +4,10 @@ const con = require('../settings/dataBaseConnection');
 
 const route = express.Router();
 
-route.get('/', (req, res) => {
-    var email = String (req.body.email);
-    var passwd = String (req.body.passwd);
-    console.log(email+"---"+passwd);
-    var q = `select * from users where email='${email}' and passwd='${passwd}'`;
+route.post('/', (req, res) => {
+    let email = req.body.email;
+    console.log(email+"---");
+    var q = `select * from users where email="${email}"`;
     try
     {
         con.query(q, (err, result) => {
@@ -20,13 +19,48 @@ route.get('/', (req, res) => {
             else
             {
                 console.log(result);
-                res.json(result);
+                console.log("Length: "+(result.length));
+                if(result.length == 0)
+                {
+                    res.send("Empty");
+                }
+                else{
+                    res.send("Present");
+                }
             }
         });
     }
     catch(err)
     {
         console.log("Database ERROR --- "+err);
+    }
+});
+
+route.post("/register", (req, res) => {
+    let imgurl = req.body.imageUrl;
+    let name = req.body.name;
+    let email = req.body.email
+    console.log(imgurl);
+    console.log(name);
+
+    try
+    {
+        let q = `insert into users values(null, '${imgurl}', '${name}', '${email}', "not given", "viewer")`;
+        con.query(q, (err, result) => {
+            if(err)
+            {
+                console.log("Error Occurred while inserting --- "+err);
+            }
+            else
+            {
+                console.log(result);
+                res.send("ok");
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log("Error Occurred --- "+err);
     }
 });
 

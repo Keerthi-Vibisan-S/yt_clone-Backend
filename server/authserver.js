87afaac98c4f2138f04c530b559936bd285210ff
Keerthi Vibisan <keerthivibisan.ct20@bitsathy.ac.in>
2022-02-20 18:51:12 +0530

    🟢 Google JWT Verification added

--- server/authserver.js
+++ server/authserver.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+//! Google Authentication
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '852762241490-gr45nghc45rkvjp5bs3uqvr4q0qkp80h.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("This is authentication Server SECURED");
     res.end();
 })
 
-// app.get("/getToken", (req, res) => {
-
-// })
+app.post("/getToken", (req, res) => {
+    const token = req.body.token;
+    console.log(token);
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        user.email = payload.email;
+        user.emailVerify = payload.email_verified;
+        console.log(payload);
+      }
+      verify().then(() => {
+        res.json(user);
+        res.end();
+      })
+      .catch(console.error);
+})
 
 app.listen(2023, () => {
     console.log("Auth Server Started");

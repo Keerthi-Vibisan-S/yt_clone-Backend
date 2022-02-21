dc4cabc39e9e9da18633b88ff1d42dce407274d4
Keerthi Vibisan <keerthivibisan.ct20@bitsathy.ac.in>
2022-02-21 20:33:52 +0530

    Our own JWT Token Generated and Sent as Response

--- server/authserver.js
+++ server/authserver.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const con = require('../settings/dataBaseConnection');
 
 //! Google Authentication
 const {OAuth2Client} = require('google-auth-library');
+const { isFunc } = require('express-fileupload/lib/utilities');
 const CLIENT_ID = '852762241490-gr45nghc45rkvjp5bs3uqvr4q0qkp80h.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
 
@@ -18,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.post("/getToken", (req, res) => {
     const token = req.body.token;
+    const email = req.body.email;
     console.log(token);
     let user = {};
     async function verify() {
@@ -29,14 +33,57 @@ app.post("/getToken", (req, res) => {
         const userid = payload['sub'];
         user.email = payload.email;
         user.emailVerify = payload.email_verified;
-        console.log(payload);
       }
       verify().then(() => {
-        res.json(user);
-        res.end();
+        if(user.email == email)
+        {
+            CheckPresent(res, email);
+        }
       })
       .catch(console.error);
 })
+
+//?Checking Database for the present user
+
+function CheckPresent(res, email)
+{
+    let q = `select * from users where email = "${email}"`;
+    let data;
+    con.query(q, (err, result) => {
+        if(err)
+        {
+            console.log(err);
+            return("");
+        }
+
+        else
+        {
+            console.log("-------- WE got Result --------");
+            console.log(result);
+            if(result.length == 0)
+            {
+                //! Register
+                console.log("No data Register");
+            }
+
+            else
+            {
+                GenerateToken(res, email);
+            }
+        }
+    })
+}
+
+//TODO: CREATING TOKEN
+function GenerateToken(res, email)
+{
+    const token = jwt.sign({email: email}, process.env.SECRET_KEY);
+    console.log("Generated Token --- "+token);
+    res.json(token);
+    res.end();
+}
+
+
 
 app.listen(2023, () => {
     console.log("Auth Server Started");
